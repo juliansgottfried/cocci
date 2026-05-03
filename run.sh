f0afb4aa3e8ca1275ebf8e6473a35a1e5923c9c2
Julian Gottfried <juliansg@princeton.edu>
2026-05-03 00:43:26 -0700

    change files

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sbatch -N 2 --ntasks-per-node=2 script.jl
+sbatch --time=20 -N 2 --ntasks-per-node=2 --mail-user=jgottf@stanford.edu script.jl
--- script.jl
+++ script.jl
@@ -2,7 +2,17 @@
 
 using Distributed, SlurmClusterManager
 addprocs(SlurmManager())
-@everywhere println("hello from $(myid()):$(gethostname())")
-all_results = pmap(1:20) do index
-	println("index: $(index), id: $(myid()), host: $(gethostname())")
+
+@everywhere include("estimate.jl")
+@everywhere include("generate.jl")
+@everywhere using JLD2
+
+@everywhere n = 25
+@everywhere l1 = 50
+@everywhere m = 1000
+@everywhere ρs = 0:0.1:10
+
+pmap(ρs) do ρ
+	results = estimate.montecarlo(n, l1, ρ, m)
+    save_object(string("/scratch/users/jgottf/cocci/results/results_",replace(string(ρ), "." => "_"), ".jld2"), [results, (n, l1, m, ρ)])
 end
