96baeab23285f364e350fa923283be8742a0c0b5
Julian Gottfried <juliansg@princeton.edu>
2026-05-04 23:30:21 -0700

    plotting code

--- testernew.jl
+++ testernew.jl
@@ -1,5 +1,7 @@
 using Plots
+using Measures
 using JLD2
+import Statistics
 
 include("estimate.jl")
 include("generate.jl")
@@ -13,12 +15,15 @@ end
 
 plotter = zeros(Float64, length(ρs) - 1, 3)
 for i in eachindex(ρs)[2:end]
-    plotter[i, :] = quantile(log(collect[i] / ρs[i]), [0.055, 0.5, 0.945])
+    plotter[i - 1, :] = Statistics.quantile(log2.(collect[i] / ρs[i]), [0.055, 0.5, 0.945])
 end
+plotter[plotter .== -Inf] .= minimum(plotter[plotter .!= -Inf])
 
-plot(ρs[2:end],  plotter[:, 1], fillrange = plotter[:, 3], 
-    xlabel = "ρ", ylabel = "log % error",
-    fillalpha = 0.15, c = "#29a0c8", linecolor = false,
-    label = false, grid = false)
+plot(ρs[2:end], plotter[:, 1], fillrange = plotter[:, 3], 
+    xlabel = "recombination rate ρ", ylabel = "log₂ ρ hat / ρ",
+    title = "Estimation error",
+    xlim = [0, 15], fillalpha = 0.15, c = "#29a0c8",
+    linecolor = false, label = false, grid = false)
+hline!([0], c = :red, alpha = 0.7, label = false)
 plot!(ρs[2:end], plotter[:, 2],
-    c = :black, label = false)
+    c = :black, linewidth = 1.2, label = false)
