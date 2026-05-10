1dfaf0d50a6d775be820df1739323c76e91143f9
Julian Gottfried <juliansg@princeton.edu>
2026-05-09 19:54:14 -0700

    minor

--- submitprob.sh
+++ submitprob.sh
@@ -1,10 +1,10 @@
 ml julia
 
 sbatch \
-    --nodes=10 \
-    --ntasks-per-node=50 \
-    --mem=10G \
-    --time=24:00:00 \
+    --nodes=5 \
+    --ntasks-per-node=100 \
+    --mem=5G \
+    --time=12:00:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
     --mail-type=ALL \
