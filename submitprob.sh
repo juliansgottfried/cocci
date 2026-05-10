7571f62dea3e83d7d93aa755d26688f7522b0610
Julian Gottfried <juliansg@princeton.edu>
2026-05-10 09:33:36 -0700

    tweaks

--- submitprob.sh
+++ submitprob.sh
@@ -3,7 +3,7 @@ ml julia
 sbatch \
     --nodes=5 \
     --ntasks-per-node=50 \
-    --mem=30G \
+    --mem=25G \
     --time=12:00:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
