2fea56d0deacc08c624d5739d4cc15c1a5e196dc
Julian Gottfried <juliansg@princeton.edu>
2026-05-11 20:18:06 -0700

    ok

--- submitdata.sh
+++ submitdata.sh
@@ -4,7 +4,7 @@ sbatch \
     --nodes=5 \
     --ntasks-per-node=50 \
     --mem=50G \
-    --time=12:00:00 \
+    --time=06:00:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
     --mail-type=ALL \
