8d39343473f1a308d799a3906fc0120fd56f2b18
Julian Gottfried <juliansg@princeton.edu>
2026-05-13 15:43:03 -0700

    changes

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 scp -r \
-	jgottf@login.sherlock.stanford.edu:/scratch/users/jgottf/cocci/prob/run_5_13_26/* \
-	prob/run_5_13_26
+	jgottf@login.sherlock.stanford.edu:/scratch/users/jgottf/cocci/data/run_5_13_26/* \
+	data/run_5_13_26
--- submitdata.sh
+++ submitdata.sh
@@ -3,8 +3,8 @@ ml julia
 sbatch \
     --nodes=5 \
     --ntasks-per-node=10 \
-    --mem=20G \
-    --time=03:00:00 \
+    --mem=30G \
+    --time=01:30:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
     --mail-type=ALL \
