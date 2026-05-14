10a6e233a29b7f608cb771899d82f93f59a725d1
Julian Gottfried <juliansg@princeton.edu>
2026-05-13 23:45:53 -0700

    changes

--- someplots.jl
+++ someplots.jl
@@ -7,31 +7,30 @@ include("generate.jl")
 
 dρ = 0.1
 maxρ = 10
-nρ = nρ = floor(Int, maxρ / dρ) + 1
+nρ = floor(Int, maxρ / dρ) + 1
 
-data0 = [load_object(generate.getfilenamelocal("data", "5_13_26", true, ρ)) for ρ in 0:dρ:maxρ]
-data1 = [load_object(generate.getfilenamelocal("data", "5_13_26", false, ρ)) for ρ in 0:dρ:maxρ]
+data0 = [load_object(generate.getfilenamelocal("data", "5_14_26", true, ρ)) for ρ in 0:dρ:maxρ]
+data1 = [load_object(generate.getfilenamelocal("data", "5_14_26", false, ρ)) for ρ in 0:dρ:maxρ]
 
 ρs = 0:dρ:maxρ
 
 aic = zeros(Float64, nρ, 2, 3)
 ratio = zeros(Float64, nρ, 4, 3)
+qs = [0.055, 0.5, 0.945]
 for i in 1:nρ
     # i = 1
-    aic[i, 1, :] = Statistics.quantile(data0[i][:, 2] - data0[i][:, 4], 
-        [0.055, 0.5, 0.945])
-    aic[i, 2, :] = Statistics.quantile(data1[i][:, 4] - data1[i][:, 2], 
-        [0.055, 0.5, 0.945])
+    aic[i, 1, :] = Statistics.quantile(data0[i][:, 2] - data0[i][:, 4], qs)
+    aic[i, 2, :] = Statistics.quantile(data1[i][:, 4] - data1[i][:, 2], qs)
     if ρ[i] == 0 
         ratio[i, 1, :] = [Inf; Inf; Inf]
         ratio[i, 2, :] = [Inf; Inf; Inf]
         ratio[i, 3, :] = [Inf; Inf; Inf]
         ratio[i, 4, :] = [Inf; Inf; Inf]
     else
-        ratio[i, 1, :] = Statistics.quantile(log2.(data0[i][:, 1] ./ ρs[i]), [0.055, 0.5, 0.945])
-        ratio[i, 2, :] = Statistics.quantile(log2.(data0[i][:, 3] ./ ρs[i]), [0.055, 0.5, 0.945])
-        ratio[i, 3, :] = Statistics.quantile(log2.(data1[i][:, 1] ./ ρs[i]), [0.055, 0.5, 0.945])
-        ratio[i, 4, :] = Statistics.quantile(log2.(data1[i][:, 3] ./ ρs[i]), [0.055, 0.5, 0.945])
+        ratio[i, 1, :] = Statistics.quantile(log2.(data0[i][:, 1] ./ ρs[i]), qs)
+        ratio[i, 2, :] = Statistics.quantile(log2.(data0[i][:, 3] ./ ρs[i]), qs)
+        ratio[i, 3, :] = Statistics.quantile(log2.(data1[i][:, 1] ./ ρs[i]), qs)
+        ratio[i, 4, :] = Statistics.quantile(log2.(data1[i][:, 3] ./ ρs[i]), qs)
     end
 end
 ratio[ratio .== -Inf] .= minimum(ratio[ratio .!= -Inf])
--- submitdata.sh
+++ submitdata.sh
@@ -3,8 +3,8 @@ ml julia
 sbatch \
     --nodes=5 \
     --ntasks-per-node=10 \
-    --mem=30G \
-    --time=04:00:00 \
+    --mem=15G \
+    --time=01:30:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
     --mail-type=ALL \
