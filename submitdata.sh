89fb4dd0e77d4b3d387367669b07732cdeff64c6
Julian Gottfried <juliansg@princeton.edu>
2026-05-14 23:30:45 -0700

    changes

--- submitdata.sh
+++ submitdata.sh
@@ -3,8 +3,8 @@ ml julia
 sbatch \
     --nodes=5 \
     --ntasks-per-node=20 \
-    --mem=20G \
-    --time=03:00:00 \
+    --mem=25G \
+    --time=05:00:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --error=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
