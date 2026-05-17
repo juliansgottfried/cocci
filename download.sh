7445af9042da49f2c3fd71dd4568916ac8dd8d36
Julian Gottfried <juliansg@princeton.edu>
2026-05-17 00:42:20 -0700

    ok

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 scp -r \
-	jgottf@login.sherlock.stanford.edu:/scratch/users/jgottf/cocci/prob/run_5_16_26_j/* \
-	prob/run_5_16_26_j
+	jgottf@login.sherlock.stanford.edu:/scratch/users/jgottf/cocci/prob/run_5_17_26_a/* \
+	prob/run_5_17_26_a
