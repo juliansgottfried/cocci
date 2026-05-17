b76d4321cf5ec0b635228c3f6c026811c7395514
Julian Gottfried <juliansg@princeton.edu>
2026-05-17 09:32:57 -0700

    ok

--- submitprob.sh
+++ submitprob.sh
@@ -4,7 +4,7 @@ sbatch \
     --nodes=5 \
     --ntasks-per-node=20 \
     --mem=10G \
-    --time=00:30:00 \
+    --time=01:00:00 \
     --output=/scratch/users/jgottf/cocci/output/%j.out \
     --error=/scratch/users/jgottf/cocci/output/%j.out \
     --partition=normal,hns \
