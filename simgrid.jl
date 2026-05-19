fdc4ee54bbfccc9093ce55cff7139815eade9bd5
Julian Gottfried <juliansg@princeton.edu>
2026-05-19 10:14:34 -0700

    ok

--- simgrid.jl
+++ simgrid.jl
@@ -7,13 +7,12 @@ dρ = 1
 maxρ = 10 - dρ
 nρ = length(0:dρ:maxρ)
 ρs = 0:dρ:maxρ
-J = 500
 
 # Retrodiction plots
 
 loadit(filename) = if isfile(filename) return load_object(filename) end
 
-data = [load_object(generate.getfilenamegridlocal("data", "5_18_26_e", ρ0, ρ1)) for ρ0 in ρs, ρ1 in ρs]
+data = [load_object(generate.getfilenamegridlocal("data", "5_19_26_a", ρ0, ρ1)) for ρ0 in ρs, ρ1 in ρs]
 skip = isnothing.(data)
 
 data[1,1]
@@ -30,34 +29,17 @@ for i in 1:nρ, j in 1:nρ
     end
 end
 
-rawplot = function(i, j)
-    k = 2(i - 1) + j
-    part1 = i == 1 ? "constant data" : "varying data"
-    part2 = j == 1 ? "constant model" : "varying model"
-    plot(ρs, raw[:, k, 1], fillrange = raw[:, k, 3], 
+plotit = function(i)
+    title = i == 1 ? "intercept" : "slope"
+    dat = i == 1 ? val[:, :, i, 2] : val[:, :, i, 2]'
+    plot(ρs, dat,
+        xlim = [0, maxρ], ylim = [0, maxρ],
         xlabel = "recombination rate", ylabel = "estimate",
-        title = "$part1, $part2",
-        xlim = [0, maxρ], fillalpha = 0.15, c = "#29a0c8",
-        linecolor = false,
-        label = false, grid = false)
-    plot!(ρs, maxρ * ones(nρ),
-        linestyle = :dash, color = "#29a0c8", label = false)
-    plot!(ρs, zeros(nρ), 
-        linestyle = :dash, color = "#29a0c8", label = false)
-    plot!(ρs, raw[:, k, 2], c = :black, 
-        linewidth = 1.2, label = false)
+        alpha = 0.5, color = :black, linewidth = 0.5,
+        title = title, label = false, grid = false)
     plot!([0, maxρ], [0, maxρ], 
         color = :red, label = false)
 end
 
-medianρ0 = val[:, :, 1, 2]
-medianρ1 = val[:, :, 2, 2]'
-
-plot(ρs, medianρ0,
-    xlabel = "recombination rate", ylabel = "estimate",
-    title = "ρ0",
-    color = :black, label = false)
-plot(ρs, medianρ1, 
-    xlabel = "recombination rate", ylabel = "estimate",
-    title = "ρ1",
-    color = :black, label = false)
+plotit(1)
+plotit(2)
