560c002365900fdeefcbc49b2a74cb4acbda49bc
Julian Gottfried <juliansg@princeton.edu>
2026-05-20 01:08:42 -0700

    ok

--- simgridbayes.jl
+++ simgridbayes.jl
@@ -16,21 +16,47 @@ data = [loadit(generate.getfilenamegridlocal("data", "5_19_26_c", ρ0, ρ1)) for
 skip = isnothing.(data)
 
 unifprior = ones(Float64, nρ) ./ nρ
-λ = 1
-expprior = λ .* exp.(-λ .* ρs)
-expprior ./= sum(expprior)
-plot(ρs, expprior, xlabel = "ρ", ylabel = "density",
+
+getprior = function(λ)
+    expprior = λ .* exp.(-λ .* ρs)
+    expprior ./= sum(expprior)
+    expprior
+end
+
+plot(ρs, getprior(1), xlabel = "ρ", ylabel = "density",
     label = false, color = :black, grid = false)
 
-get
-
-for i in 1:nρ, j in 1:nρ
-    agg = ones(Float64, nρ, nρ)
-    for k in 1:J
-        agg .*= data[i, j][k, :, :]
-    end
-
+jointprior = function(prior)
+    [prior[i] * prior[j] for i in 1:nρ, j in 1:nρ]
 end
+
+getstats = function(i, j, prior)
+    tmpdat = data[i, j]
+    agg = log.(jointprior(prior))
+    for k in 1:J
+        agg .+= tmpdat[k, :, :]
+    end
+    agg .-= sum(agg)
+    agg
+end
+
+plotheat2 = function(i, j, prior)
+    heatmap(ρs, ρs, getstats(i, j, prior)',
+        xlabel = "ρ0", ylabel = "ρ1",
+        xlim = [0, maxρ], ylim = [0, maxρ],
+        c = cgrad(:diff, rev = true), 
+        title = "ρ0: $ρ0, ρ1: $ρ1",
+        aspect_ratio = 1, colorbar_title = "log P")
+end
+
+plotp0 = [sum(getstats(i, 1, unifprior)[:, 1]) for i in 1:nρ]
+plot(ρs, plotp0,
+    xlabel = "ρ0", ylabel = "P(ρ1 = 0)",
+    label = false, grid = false, color = :blacl)
+
+plotheat2(1, 1, unifprior)
+plotheat2(1, 1, getprior(1))
+
 
 val = zeros(Float64, nρ, nρ, 2, 3)
 qs = [0.055, 0.5, 0.945]
