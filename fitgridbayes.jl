5c8859f118bdfab1297e861ff0f4e7d4bd8ab477
Julian Gottfried <juliansg@princeton.edu>
2026-05-20 10:51:54 -0700

    ok

---
+++ fitgridbayes.jl
@@ -0,0 +1,61 @@
+using DelimitedFiles, Plots
+import StatsBase, StatsPlots, Distributions
+
+include("estimate.jl")
+include("generate.jl")
+include("kernels.jl")
+
+dρ = 0.5
+maxρ = 20 - dρ
+nρ = length(0:dρ:maxρ)
+ρs = 0:dρ:maxρ
+
+n = 17
+
+gather = [load_object(generate.getfilenamegridlocal("prob", "5_19_26_a", ρ0, ρ1)) for ρ0 in ρs, ρ1 in ρs]
+pseudo = estimate.getpseudo(gather, n)
+
+covariate = readdlm("rodent_data/covariate.csv", ',', Any, '\n')
+alleles = readdlm("sampling_data/alleles.csv", ',', Any, '\n')
+
+chunksize = Int(floor(maximum(alleles[:, end]) / 500))
+chunkidx = 2580020
+chunkloci = (alleles[:, end] .>= chunkidx) .& (alleles[:, end] .<= chunkidx + chunksize)
+chunk = alleles[chunkloci, :]
+chunk[:, end] .-= minimum(chunk[:, end]) - 1
+
+nloci = size(chunk)[1]
+nsample = 50
+window = 10000
+nwindow = chunksize - window
+
+# G = 365
+# η = 0.95
+# maxbrk = 50
+# pvec = kernels.getkernels(G, η, maxbrk)
+pvec = 1
+
+S = 500
+ρhat = zeros(Float64, S, nρ, nρ)
+for i in 1:S
+    subset = falses(nloci)
+    println(i)
+    while sum(subset) < nsample
+        windowidx = StatsBase.sample((1:nwindow), 1)[1]
+        subset = (chunk[:, end] .>= windowidx) .& (chunk[:, end] .<= windowidx + window)
+    end
+    sampleidx = StatsBase.sample((1:nloci)[subset], nsample, replace = false)
+    samples = chunk[sampleidx, :]
+
+    configs = zeros(Int, Int(nsample * (nsample - 1) / 2), 3)
+    dists = zeros(Float64, size(configs)[1])
+    for i in 2:nsample
+        for j in 1:(i - 1)
+            c = Int((i * (i - 3)) / 2 + 1 + j)
+            configs[c, :] = generate.getconfig(samples[[i; j], 1:(end - 1)])
+            dists[c] = abs(samples[i, end] - samples[j, end]) / window
+        end
+    end
+    ρhat[i, :, :] = estimate.getlgrid(n, gather, pseudo,
+            dρ, maxρ, configs, dists, pvec)
+end
--- simgridbayes.jl
+++ simgridbayes.jl
@@ -3,54 +3,111 @@ using Plots, JLD2, StatsBase
 include("estimate.jl")
 include("generate.jl")
 
+logsumexp = function(x)
+    xmax = maximum(x)
+    xmax + log(sum(exp.(x .- xmax)))
+end
+
 dρ = 0.5
 maxρ = 20 - dρ
 nρ = length(0:dρ:maxρ)
 ρs = 0:dρ:maxρ
+J = 500
 
 loadit(filename) = if isfile(filename) return load_object(filename) end
 data = [loadit(generate.getfilenamegridlocal("data", "5_19_26_c", ρ0, ρ1)) for ρ0 in ρs, ρ1 in ρs]
 skip = isnothing.(data)
 
-unifprior = ones(Float64, nρ) ./ nρ
+lunifprior = log.(ones(Float64, nρ) ./ nρ)
 
-getprior = function(λ)
-    expprior = λ .* exp.(-λ .* ρs)
-    expprior ./= sum(expprior)
-    expprior
+lgetprior = function(λ)
+    expprior = log(λ) .- λ .* ρs
+    expprior .- logsumexp(expprior)
 end
 
-plot(ρs, getprior(1), xlabel = "ρ", ylabel = "density",
-    label = false, color = :black, grid = false)
+# plot(ρs, lgetprior(10), xlabel = "ρ", ylabel = "density",
+#     label = false, color = :black, grid = false)
 
-jointprior = function(prior)
-    [prior[i] * prior[j] for i in 1:nρ, j in 1:nρ]
+jointprior = function(prior0, prior1)
+    [prior0[i] + prior1[j] for i in 1:nρ, j in 1:nρ]
 end
 
-getstats = function(i, j, prior)
-    tmpdat = data[i, j]
-    agg = log.(jointprior(prior))
-    for k in 1:J
+getstats = function(i, j, ndata, real)
+    if real tmpdat = ρhat
+    else tmpdat = data[i, j]
+    end
+    agg = jointprior(prior0, prior1)
+    for k in 1:ndata
         agg .+= tmpdat[k, :, :]
     end
-    agg .-= sum(agg)
-    agg
+    agg .- logsumexp(agg)
 end
 
-plotheat = function(i, j, prior)
-    heatmap(ρs, ρs, getstats(i, j, prior)',
+plotheat = function(i, j, cutoff, ndata, real)
+    ρ0 = ρs[i]
+    ρ1 = ρs[j]
+    vals = getstats(i, j, ndata, real)
+    vals[vals .< cutoff] .= cutoff
+    heatmap(ρs, ρs, vals',
         xlabel = "ρ0", ylabel = "ρ1",
         xlim = [0, maxρ], ylim = [0, maxρ],
-        c = cgrad(:diff, rev = true), 
+        c = cgrad(:default, rev = false), 
         title = "ρ0: $ρ0, ρ1: $ρ1",
         aspect_ratio = 1, colorbar_title = "log P")
 end
 
-plotp0 = [sum(getstats(i, 1, unifprior)[:, 1]) for i in 1:nρ]
-# plotp0 = [sum(getstats(i, 1, getprior(1))[:, 1]) for i in 1:nρ]
-plot(ρs, plotp0,
-    xlabel = "ρ0", ylabel = "P(ρ1 = 0)",
-    label = false, grid = false, color = :blacl)
+plotheatall = function(idx, ndata)
+    best = zeros(Float64, nρ, nρ)
+    for i in 1:nρ, j in 1:nρ
+        if skip[i, j] best[i, j] = Inf 
+        else
+            vals = getstats(i, j, ndata, false)
+            best[i, j] = ρs[argmax(vals)[idx]]
+        end
+    end
+    heatmap(ρs, ρs, best',
+        xlabel = "ρ0", ylabel = "ρ1",
+        xlim = [0, maxρ], ylim = [0, maxρ],
+        c = cgrad(:default, rev = false),
+        clim = (0, 20), grid = false,
+        aspect_ratio = 1, colorbar_title = "ρ0 hat")
+end
 
-plotheat(1, 1, unifprior)
-# plotheat(1, 1, getprior(1))
+plotthresh = function(cutoff, use, ndata)
+    p0 = zeros(Float64, nρ, nρ)
+    for i in 1:nρ, j in 1:nρ
+        if skip[i, j] best[i, j] = Inf 
+        else
+            vals = getstats(i, j, ndata, false)
+            p0[i, j] = logsumexp(vals[:, 1])
+        end
+    end
+    if use p0 = p0 .< cutoff end
+    heatmap(ρs, ρs, p0,
+        xlabel = "ρ0", ylabel = "ρ1",
+        xlim = [0, maxρ], ylim = [0, maxρ],
+        c = cgrad(:default, rev = false),
+        aspect_ratio = 1, colorbar_title = "log p0")
+end
+
+# plots
+
+ndata = 500
+λ0 = 100
+λ1 = 1
+thresh = -7000
+prior0 = lgetprior(λ0)
+prior1 = lgetprior(λ1)
+
+plotthresh(thresh, true, ndata)
+plotthresh(thresh, false, ndata)
+
+# plotheat(10, 1, thresh, ndata, false)
+plotheatall(1, ndata)
+plotheatall(2, ndata)
+
+# data
+plotheat(1, 1, thresh, ndata, true)
+
+largeeffect = logsumexp(getstats(1, 1, ndata, true)[:, 1])
+p0 .< cutoff
